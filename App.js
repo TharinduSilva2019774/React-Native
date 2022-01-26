2e55c0bd86156bca8415cf6c3879b05772421341
Tharindu Silva <78898203+TharinduSilva2019774@users.noreply.github.com>
2022-01-26 09:35:18 +0530

    Custom button
    Created a custom button component

--- App.js
+++ App.js
@@ -3,12 +3,23 @@ import { useDimensions } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import VeiwImageScreen from './app/screens/VeiwImageScreen';
 
-import AppText from './app/components/AppText/AppText';
+import AppText from './app/components/AppText';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
 
   return (
-  <AppText>ergegg</AppText>
+    <View style={styles.view}>
+      <AppButton title={"Login"} onPress={()=>console.log("Custom button works")}></AppButton>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:'center',
+  }
+})
 
